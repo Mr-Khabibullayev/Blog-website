b6b0a6a8f94669ac2589841f6d9c1cc69341fd69
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2022-12-19 20:44:34 +0500

    created post.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogLIstView
+from .views import BlogLIstView,BlogDetailView
 
 urlpatterns = [
-    path('',BlogLIstView.as_view(),name='home')
+    path('',BlogLIstView.as_view(),name='home'),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post
 
 # Create your views here.
 class BlogLIstView(ListView):
     model = Post
     template_name = 'home.html'
+    
+    
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
